e1536d47615588ed93ec68a42ecb09f3ebd32d9a
Bartek0074 <bartekdo123@gmail.com>
2026-05-19 22:52:47 +0200

    chore: added kpo pdf

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,3 @@
-import Heading from '../Heading/Heading';
 import styles from './Footer.module.scss';
 
 import { FaFacebook } from 'react-icons/fa';
@@ -7,16 +6,32 @@ const Footer = () => {
 	const currentYear = new Date().getFullYear();
 
 	return (
-		<footer className={styles.footer}>
-			<p className={styles.text}>
-				<span>&copy; {currentYear} | Winnica Rubinowa | Cezary Morek | </span>
-			</p>
-			<div className={styles.link}>
-				<a href='https://www.facebook.com/WinnicaRubinowa/' target='_blank'>
-					<FaFacebook className={styles.icon} />
+		<>
+			<footer className={styles.footer}>
+				<p className={styles.text}>
+					<span>&copy; {currentYear} </span>
+					<span>|</span>
+					<span>Winnica Rubinowa</span>
+					<span>|</span>
+					<span>Cezary Morek</span>
+					<span>|</span>
+				</p>
+				<div className={styles.link}>
+					<a href='https://www.facebook.com/WinnicaRubinowa/' target='_blank'>
+						<FaFacebook className={styles.icon} />
+					</a>
+				</div>
+			</footer>
+			<div className={styles.subFooter}>
+				<a
+					href="/prezentacja_kpo.pdf"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Projekt KPO
 				</a>
 			</div>
-		</footer>
+		</>
 	);
 };
 
